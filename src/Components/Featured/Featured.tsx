64a63543ae3ede401eb67f9fd62e3e7cb1fa5750
hunor64 <nhunor06@gmail.com>
2025-04-10 10:16:47 +0200

    Updated

--- src/Components/Featured/Featured.tsx
+++ src/Components/Featured/Featured.tsx
@@ -1,15 +1,19 @@
 
+import { useState } from 'react'
 import './featured.css'
 
-import FeatureCard from './featuredCard/FeatureCard'
+import FeatureCard, { FeatureCardProps } from './featuredCard/FeatureCard'
 
-{/* Fetcheld be az adatokat a featured jsonból és jelenítsd meg a featured_container elemből*/}
 
 {/* Állítsd be a featured.css-ben, hogy az órák 992px szélesség alatt egymás alá kerüljenek a fölött pedig egymás mellé */}
 
 
 const Featured = () => {
-
+  const [featured, setFeatured] = useState<FeatureCardProps[]>([])
+  fetch('/featured.json')
+    .then((response) => response.json())
+    .then((data) => setFeatured(data))
+    .catch((error) => console.error('Error fetching data:', error))
 
 
   return (
@@ -17,6 +21,9 @@ const Featured = () => {
     <h2 className="feature__title">Featured</h2>
     <div className="featured__container">
             {/* feauterd watches */}
+            {featured.map((item, index) => (
+                <FeatureCard key={index} title={item.title} price={item.price} image={item.image} />
+            ))}
     </div>
 </section>
   )
--- src/Components/Featured/featuredCard/FeatureCard.tsx
+++ src/Components/Featured/featuredCard/FeatureCard.tsx
@@ -3,21 +3,19 @@ import './featurecard.css'
 export type FeatureCardProps = {
   title: string,
   price: string,
-  img: string,
+  image: string,
 }
 
-{/*Egészítsd ki a komponenst a megfelelő props-ok használatával */}
-
-const FeatureCard = () => {
+const FeatureCard = ({ title, price, image }: FeatureCardProps) => {
   return (
     <div className="featured__card">
         <span className="featured__tag">Sale</span>
     
-        <img src="" alt="" className="featured__img"/>
+        <img src={image} alt={title} className="featured__img"/>
     
         <div className="featured__data">
-            <h3 className="featured__title"></h3>
-            <span className="featured__price"></span>
+            <h3 className="featured__title">{title}</h3>
+            <span className="featured__price">{price}</span>
         </div>
     
         <button className="button featured__button">ADD TO CART</button>
