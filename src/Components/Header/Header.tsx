54870111df847459d83021b03183bcead7dc6064
hunor64 <nhunor06@gmail.com>
2025-04-10 10:32:58 +0200

    Updated

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -5,18 +5,13 @@ import { useState } from 'react';
 
 
 const Header = () => {
-    
-    {/*Adj a theme ikonhoz egy olyan funkciót amivel váltogatni lehet a light és dark mode között*/}
-    
+        
   {/*Adj egy funkciót a toggle ikonhoz amivel mobilnézetben le lehet nyitni a navigációs menüt a close ikonnal meg bezárni*/}
   
   {/*
     Adj egy funkciót a cart-shop ikonhoz amivel le lehet nyitni a vásárlási listát
     A vásárlási lista egy külön komponens.
-    */}
-    
-    {/* Állítsd be az App.css-ben az ul osztályszelektornak, hogy a listaelemek pontok nélkül jelenjenek meg */}
-    
+    */}    
     const [isCartVisible, setIsCartVisible] = useState(false);
 
   return (
@@ -28,18 +23,25 @@ const Header = () => {
         </a>
         <div className="nav__menu"  id="nav-menu">      
             <ul className="nav__list">
-                {/*
-                Készítsd el a Header-eket: Home, Featured, Products, New
-                Mindegyik egy listaelem, és azon belül egy hivatkozás
-                a listaelem ostrálya nav__item, a hivatkozás osztálya nav__link
-                */}
+                <li className="nav__item">
+                    <a href="#home" className="nav__link">Home</a>
+                </li>
+                <li className="nav__item">
+                    <a href="#featured" className="nav__link">Featured</a>
+                </li>
+                <li className="nav__item">
+                    <a href="#products" className="nav__link">Products</a>
+                </li>
+                <li className="nav__item">
+                    <a href="#new" className="nav__link">New</a>
+                </li>
             </ul>
             <div className="nav__close" id="nav-close">
                 <i className='bx bx-x' ></i>
             </div>
         </div>
         <div className="nav__btns">           
-            <i className='bx bx-moon change-theme' id="theme-button"></i>
+            <i className='bx bx-moon change-theme' onClick={()=>document.body.classList.toggle("dark-theme")} id="theme-button"></i>
             <div className="nav__shop" id="cart-shop" >
                 <i className='bx bx-shopping-bag' onClick={()=>setIsCartVisible(!isCartVisible)}></i>
             </div>
@@ -49,7 +51,6 @@ const Header = () => {
         </div>
     </nav>
 </header>
-{/*Itt jelenjen meg a Cart ha az ikonra kattintottunk */}
 {
     isCartVisible &&
         <Cart/>
